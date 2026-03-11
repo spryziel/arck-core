1d43a95c14edad2903573de0244118e2f28d75b2
Spryziel <spryziel@gmail.com>
2026-03-11 11:28:54 +0000

    Implemented HTTP response transmission via send system call.

--- core.cpp
+++ core.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+
 int main() {
+    // 1. THE GATEWAY
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     std::cout << server_fd << "\n";
+
+    // 2. THE MEMORY MAP & THE ANCHOR
     struct sockaddr_in proxy_address;
     proxy_address.sin_family = AF_INET;
     proxy_address.sin_port = htons(8080);
     proxy_address.sin_addr.s_addr = INADDR_ANY;
+    
     int bind_status = bind(server_fd, (struct sockaddr*)&proxy_address, sizeof(proxy_address));
     std::cout << bind_status << "\n";
+
+    // 3. THE SENSOR
     int listen_status = listen(server_fd, 10);
     std::cout << listen_status << "\n";
-    
+
+    // 4. THE CAPTURE (BLOCKING)
+    struct sockaddr_in client_address;
+    socklen_t client_len = sizeof(client_address);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_len);
+    std::cout << client_fd << "\n";
+
+    // 5. THE INTERROGATION
+    char buffer[1024] = {0};
+    read(client_fd, buffer, 1024);
+    std::cout << buffer << "\n";
+    const char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nARCK-CORE V1\n";
+    int response_len = sizeof(response) - 1;
+    send(client_fd, response, response_len, 0);
+
+    // 6. THE SURRENDER
+    close(client_fd);
+    return 0;
 }
