38f8bc01889aec5912195e7dee880e92071b6954
Spryziel <spryziel@gmail.com>
2026-03-11 07:50:05 +0000

    pointers

--- core.cpp
+++ core.cpp
@@ -2,8 +2,12 @@
 int main() {
     int arck_core_port = 8080;
     char proxy_flag = 'A';
+    int* leash_port = &arck_core_port;
     std::cout << sizeof(arck_core_port) << "\n";
     std::cout << sizeof(proxy_flag) << "\n";
     std::cout << &arck_core_port << "\n";
     std::cout << (void*)&proxy_flag << "\n";
+    std::cout << sizeof(leash_port) << "\n";
+    std::cout << leash_port << "\n";
+    std::cout << *leash_port << "\n";
 }
