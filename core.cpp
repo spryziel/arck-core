94a5c58e096511bbc2627ba763db9d59c305f52b
Spryziel <spryziel@gmail.com>
2026-03-11 12:21:10 +0000

    Forged infinite execution loop for continuous connection handling.

--- core.cpp
+++ core.cpp
@@ -2,40 +2,41 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
 int main() {
-    // 1. THE GATEWAY
+    
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    std::cout << server_fd << "\n";
-
-    // 2. THE MEMORY MAP & THE ANCHOR
+    
     struct sockaddr_in proxy_address;
     proxy_address.sin_family = AF_INET;
     proxy_address.sin_port = htons(8080);
     proxy_address.sin_addr.s_addr = INADDR_ANY;
     
-    int bind_status = bind(server_fd, (struct sockaddr*)&proxy_address, sizeof(proxy_address));
-    std::cout << bind_status << "\n";
+    bind(server_fd, (struct sockaddr*)&proxy_address, sizeof(proxy_address));
+    listen(server_fd, 10);
+    
+    std::cout << "ARCK-CORE LISTENING ON 8080...\n";
 
-    // 3. THE SENSOR
-    int listen_status = listen(server_fd, 10);
-    std::cout << listen_status << "\n";
+    
+    while (true) {
+        struct sockaddr_in client_address;
+        socklen_t client_len = sizeof(client_address);
+        
+        
+        int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_len);
+        
+        char buffer[1024] = {0};
+        read(client_fd, buffer, 1024);
+        
+        std::cout << "INCOMING STRIKE:\n" << buffer << "\n";
+        
+        const char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nARCK-CORE V1\n";
+        send(client_fd, response, sizeof(response) - 1, 0);
+        
+        
+        close(client_fd);
+    }
 
-    // 4. THE CAPTURE (BLOCKING)
-    struct sockaddr_in client_address;
-    socklen_t client_len = sizeof(client_address);
-    int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_len);
-    std::cout << client_fd << "\n";
-
-    // 5. THE INTERROGATION
-    char buffer[1024] = {0};
-    read(client_fd, buffer, 1024);
-    std::cout << buffer << "\n";
-    const char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nARCK-CORE V1\n";
-    int response_len = sizeof(response) - 1;
-    send(client_fd, response, response_len, 0);
-
-    // 6. THE SURRENDER
-    close(client_fd);
-    return 0;
+    return 0; 
 }
